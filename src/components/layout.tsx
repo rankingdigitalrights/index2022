428ba9ee687512524956efec60c2d77f3bd2dd5f
Christo <christo@cryptodrunks.net>
2020-11-23 20:45:22 +0100

    feat: set a HTML header for all pages in the layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 
 import Navigation from "./navigation";
@@ -9,6 +10,11 @@ interface LayoutProps {
 const Layout = ({children}: LayoutProps) => {
   return (
     <div>
+      <Head>
+        <title>Ranking Digital Rights - Index 2020</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <header className="container mx-auto pt-4 mb-8">
         <Navigation />
       </header>
