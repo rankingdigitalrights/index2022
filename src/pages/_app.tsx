ebed8f50fbb069f4a41a1157bea49efcdda5aac2
Christo <christo@cryptodrunks.net>
2020-11-23 16:40:44 +0100

    feat: only show form focus rings when actually tabbing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import "../css/index.css";
 
 import {AppProps} from "next/app";
+import React, {useEffect, useState} from "react";
 
 const MyApp = ({Component, pageProps}: AppProps) => {
+  const [firstTabHandled, setFirstTabHandled] = useState(false);
+
+  // enable form focus rings when tabbing
+  useEffect(() => {
+    if (firstTabHandled) return;
+
+    const handleFirstTab = (ev: KeyboardEvent) => {
+      // the "I am a keyboard user" key
+      if (ev.key === "Tab") {
+        document.body.classList.add("user-is-tabbing");
+        window.removeEventListener("keydown", handleFirstTab);
+      }
+    };
+
+    window.addEventListener("keydown", handleFirstTab);
+    setFirstTabHandled(true);
+  });
+
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 };
