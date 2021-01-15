ec2f10fa257bb81eb257a33ba5b772b73741ef07
Christo <christo@cryptodrunks.net>
2021-01-15 11:43:26 +0100

    refactor: render indicator company score charts as a column on mobile

--- src/components/indicator-companies-chart-container.tsx
+++ src/components/indicator-companies-chart-container.tsx
@@ -24,7 +24,7 @@ const IndicatorCompaniesChartContainer = ({
   if (hasTelecomScores && hasPlatformScores)
     return (
       <div className="flex flex-col md:flex-row md:justify-between">
-        <div className="flex flex-col pr-3 w-1/2">
+        <div className="flex flex-col pr-3 w-full md:w-1/2">
           <CompanyKindLabel kind="telecom" theme="dark" />
 
           <IndicatorCompaniesChart
@@ -34,7 +34,7 @@ const IndicatorCompaniesChartContainer = ({
           />
         </div>
 
-        <div className="flex flex-col pl-3 w-1/2">
+        <div className="flex flex-col pl-3 w-full md:w-1/2">
           <CompanyKindLabel kind="internet" theme="dark" />
 
           <IndicatorCompaniesChart
