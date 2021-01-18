eb96ec5f0f536b3a187c2b8661399198b0e9e65a
Christo <christo@cryptodrunks.net>
2021-01-18 10:54:48 +0100

    feat: add link targets to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,17 +7,59 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row md:justify-between divide-y-2 md:divide-y-0 md:divide-x-2 divide-solid divide-white">
           <div className="md:w-1/3 py-6 px-6 md:px-6 md:py-3">
             <ul className="list-none">
-              <li className="pb-1">About</li>
-              <li className="pb-1">2020 Methodology</li>
-              <li className="pb-1">Publications</li>
-              <li className="pb-1">2020 Corporate Accountability Index</li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://rankingdigitalrights.org/about/"
+                >
+                  About
+                </a>
+              </li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://rankingdigitalrights.org/methodology-development/"
+                >
+                  2020 Methodology
+                </a>
+              </li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://rankingdigitalrights.org/publications/"
+                >
+                  Publications
+                </a>
+              </li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://rankingdigitalrights.org/index2020/"
+                >
+                  2020 Corporate Accountability Index
+                </a>
+              </li>
             </ul>
           </div>
 
           <div className="md:w-1/3 py-6 px-6 md:px-6 md:py-3 md:pl-6">
             <ul className="list-none">
-              <li className="pb-1">Privacy Policy</li>
-              <li className="pb-1">Funders and partners</li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://rankingdigitalrights.org/privacypolicy"
+                >
+                  Privacy Policy
+                </a>
+              </li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://rankingdigitalrights.org/who/partners/"
+                >
+                  Funders and partners
+                </a>
+              </li>
               <li className="pb-1">
                 <a
                   rel="license"
@@ -35,9 +77,30 @@ const Footer = () => {
           <div className="md:w-1/3 py-6 px-6 md:px-6 md:py-3 md:pl-6">
             <ul className="list-none">
               <li className="pb-1">RankingRights</li>
-              <li className="pb-1">RankingRights</li>
-              <li className="pb-1">RSS</li>
-              <li className="pb-1">info@rankingdigitalrights.org</li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://twitter.com/rankingrights"
+                >
+                  RankingRights
+                </a>
+              </li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="https://rankingdigitalrights.org/feed/"
+                >
+                  RSS
+                </a>
+              </li>
+              <li className="pb-1">
+                <a
+                  className="text-white"
+                  href="mailto:info@rankingdigitalrights.org"
+                >
+                  info@rankingdigitalrights.org
+                </a>
+              </li>
             </ul>
           </div>
         </div>
