d03a23da6a04d281a46436a92f5bcb9d2360e80b
Christo <christo@cryptodrunks.net>
2021-01-18 17:18:40 +0100

    feat: structure of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,29 @@ import Layout from "../components/layout";
 const Home = () => {
   return (
     <Layout>
-      <main className="container mx-auto">Home!</main>
+      <div className="relative">
+        <div className="absolute flex flex-row w-full h-full top-0">
+          <div className="md:w-1/2 w-full md:bg-accent-pink" />
+          <div className="md:w-1/2 w-full md:bg-rdr" />
+        </div>
+
+        <div className="md:container md:mx-auto flex flex-col md:flex-row md:justify-between">
+          <div className="md:w-1/3 h-64 bg-accent-pink z-10 border">AAA</div>
+          <div className="md:w-1/3 h-64 bg-diff-add z-10 border">AAA</div>
+          <div className="md:w-1/3 h-64 bg-rdr z-10 border">AAA</div>
+        </div>
+      </div>
+
+      <div className="md:container md:mx-auto flex flex-col md:flex-row md:justify-between my-6">
+        <div className="md:w-1/3 h-64 border">AAA</div>
+        <div className="md:w-2/3 h-64 border">AAA</div>
+      </div>
+
+      <div className="md:container md:mx-auto flex flex-col md:flex-row md:justify-between">
+        <div className="md:w-1/3 h-64 border">AAA</div>
+        <div className="md:w-1/3 h-64 border">AAA</div>
+        <div className="md:w-1/3 h-64 border">AAA</div>
+      </div>
     </Layout>
   );
 };
