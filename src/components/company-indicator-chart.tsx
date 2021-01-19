02b2fa1712b1bc95fa92485242ab36f5fb9c30c7
Christo <christo@cryptodrunks.net>
2021-01-19 21:52:47 +0100

    fix: only hightlight individual company indicator charts

--- src/components/company-indicator-chart.tsx
+++ src/components/company-indicator-chart.tsx
@@ -68,78 +68,85 @@ const CompanyIndicatorChart = ({
             "flex flex-grow items-center justify-between font-circular text-xs";
 
           return (
-            <div
-              key={`company-indicator-chart-${indicator}`}
-              className={c("flex flex-col", {"mt-2": idx > 0})}
-              onMouseEnter={() => setHighlightedIndicator(indicator)}
-              onMouseLeave={() => setHighlightedIndicator(undefined)}
-            >
-              <button
-                className="flex justify-between items-center text-xs font-circular cursor-pointer"
-                onClick={
-                  hasCollapse
-                    ? () => handleCollapse(indicator)
-                    : () => onClick(display)
-                }
+            <div key={`company-indicator-chart-${indicator}`}>
+              <div
+                className={c("flex flex-col", {"mt-2": idx > 0})}
+                onMouseEnter={() => setHighlightedIndicator(indicator)}
+                onMouseLeave={() => setHighlightedIndicator(undefined)}
               >
-                <div className="flex flex-grow justify-between items-center text-left">
-                  <span className="flex-grow">{indicatorPretty}</span>
-                  {hasCollapse && isOpen && (
-                    <ChevronUp className="flex-none ml-auto ml-1" />
-                  )}
-                  {hasCollapse && !isOpen && (
-                    <ChevronDown className="flex-none ml-auto ml-1" />
-                  )}
-                </div>
-                {hasCollapse && (
-                  <div className="flex-none ml-auto w-8">&nbsp;</div>
-                )}
-              </button>
-
-              <div className={classNameBarRow}>
-                <div ref={ref} className="flex-grow">
-                  <svg
-                    version="1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="100%"
-                    height={10}
-                    transform="translate(0, 0)"
-                  >
-                    <PercentageBar
-                      value={mapScore(score)}
-                      width={chartWidth}
-                      height={9}
-                      className={
-                        isHighlightedIndicator ? highlightClassName : className
-                      }
-                    />
-                  </svg>
-                </div>
-
-                <div
-                  className={c(
-                    "flex-none ml-auto w-8 float-right",
-                    isHighlightedIndicator ? highlightClassName : undefined,
-                  )}
+                <button
+                  className="flex justify-between items-center text-xs font-circular cursor-pointer"
+                  onClick={
+                    hasCollapse
+                      ? () => handleCollapse(indicator)
+                      : () => onClick(display)
+                  }
                 >
-                  <span className="float-right">
-                    {score}
-                    {score === "NA" ? "" : "%"}
-                  </span>
+                  <div className="flex flex-grow justify-between items-center text-left">
+                    <span className="flex-grow">{indicatorPretty}</span>
+                    {hasCollapse && isOpen && (
+                      <ChevronUp className="flex-none ml-auto ml-1" />
+                    )}
+                    {hasCollapse && !isOpen && (
+                      <ChevronDown className="flex-none ml-auto ml-1" />
+                    )}
+                  </div>
+                  {hasCollapse && (
+                    <div className="flex-none ml-auto w-8">&nbsp;</div>
+                  )}
+                </button>
+
+                <div className={classNameBarRow}>
+                  <div ref={ref} className="flex-grow">
+                    <svg
+                      version="1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      height={10}
+                      transform="translate(0, 0)"
+                    >
+                      <PercentageBar
+                        value={mapScore(score)}
+                        width={chartWidth}
+                        height={9}
+                        className={
+                          isHighlightedIndicator
+                            ? highlightClassName
+                            : className
+                        }
+                      />
+                    </svg>
+                  </div>
+
+                  <div
+                    className={c(
+                      "flex-none ml-auto w-8 float-right",
+                      isHighlightedIndicator ? highlightClassName : undefined,
+                    )}
+                  >
+                    <span className="float-right">
+                      {score}
+                      {score === "NA" ? "" : "%"}
+                    </span>
+                  </div>
                 </div>
               </div>
 
               {isOpen &&
                 familyMembers.map((m) => {
                   const mChartWidth =
-                    chartWidth - 8 < 0 ? chartWidth : chartWidth - 8;
+                    chartWidth - 9 < 0 ? chartWidth : chartWidth - 9;
 
                   const mIndicatorPretty = `${m.display}. ${m.label}`;
+                  const isHighlightedMIndicator =
+                    m.indicator === highlightedIndicator;
 
                   return (
                     <div
                       key={`company-indicator-chart-${m.indicator}`}
                       className="pl-2 flex flex-col mt-2"
+                      onMouseEnter={() => setHighlightedIndicator(m.indicator)}
+                      onMouseLeave={() => setHighlightedIndicator(undefined)}
                     >
                       <button
                         className="text-left text-xs font-circular cursor-pointer"
@@ -162,7 +169,7 @@ const CompanyIndicatorChart = ({
                               width={mChartWidth}
                               height={9}
                               className={
-                                isHighlightedIndicator
+                                isHighlightedMIndicator
                                   ? highlightClassName
                                   : className
                               }
@@ -173,7 +180,7 @@ const CompanyIndicatorChart = ({
                         <div
                           className={c(
                             "flex-none ml-auto w-8 float-right",
-                            isHighlightedIndicator
+                            isHighlightedMIndicator
                               ? highlightClassName
                               : undefined,
                           )}
