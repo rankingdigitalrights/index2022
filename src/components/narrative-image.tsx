8d4ee0c5b867a42ecbf3414a867623cf83ba331f
Christo <christo@cryptodrunks.net>
2021-02-03 10:48:21 +0100

    feat: set captions to narrative images

--- src/components/narrative-image.tsx
+++ src/components/narrative-image.tsx
@@ -12,14 +12,17 @@ const NarrativeImage = ({src, alt, title}: NarrativeImageProps) => {
   const imageWebp = require(`../images/${src}?resize&webp&sizes[]=300&sizes[]=600&sizes[]=1000`);
 
   return (
-    <div className="flex justify-around">
+    <figure className="flex flex-col justify-around">
       <picture>
         <source srcSet={imageWebp.srcSet} type="image/webp" />
         <source srcSet={image.srcSet} type="image/png" />
 
         <img src={image.src} srcSet={image.srcSet} alt={alt} title={title} />
       </picture>
-    </div>
+      <figcaption className="font-circular font-bold text-sm text-right mt-1">
+        {title}
+      </figcaption>
+    </figure>
   );
 };
 
