86edc2b1e84cb54794e3da2fc7469f5c50e5cbd3
Christo <christo@cryptodrunks.net>
2021-02-05 16:19:08 +0100

    refactor: set various sizes for image scaling

--- src/components/narrative-image.tsx
+++ src/components/narrative-image.tsx
@@ -8,21 +8,30 @@ interface NarrativeImageProps {
 }
 
 const NarrativeImage = ({src, alt, title}: NarrativeImageProps) => {
-  const image = require(`../../data/images/${src}?resize&sizes[]=300&sizes[]=600&sizes[]=1000`);
-  const imageWebp = require(`../../data/images/${src}?resize&webp&sizes[]=300&sizes[]=600&sizes[]=1000`);
+  const image = require(`../../data/images/${src}?resize&sizes[]=400&sizes[]=800&sizes[]=1024&sizes[]=2048`);
+  const imageWebp = require(`../../data/images/${src}?resize&webp&sizes[]=400&sizes[]=800&sizes[]=1024&sizes[]=2048&format=webp`);
 
   return (
-    <figure className="flex flex-col justify-around my-6">
-      <picture>
-        <source srcSet={imageWebp.srcSet} type="image/webp" />
-        <source srcSet={image.srcSet} type="image/png" />
+    <div className="flex justify-around">
+      <figure className="flex flex-col justify-around my-6">
+        <picture>
+          <source srcSet={imageWebp.srcSet} type="image/webp" />
+          <source srcSet={image.srcSet} type="image/png" />
+          <source srcSet={image.srcSet} type="image/jpg" />
 
-        <img src={image.src} srcSet={image.srcSet} alt={alt} title={title} />
-      </picture>
-      <figcaption className="font-circular font-bold text-sm text-right mt-1">
-        {title}
-      </figcaption>
-    </figure>
+          <img
+            src={image.src}
+            srcSet={image.srcSet}
+            alt={alt}
+            title={title}
+            loading="lazy"
+          />
+        </picture>
+        <figcaption className="font-circular font-bold text-sm text-right mt-1">
+          {title}
+        </figcaption>
+      </figure>
+    </div>
   );
 };
 
