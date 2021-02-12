003f7f332ecc5057235130ddfb5e0d31b4165c39
Christo <christo@cryptodrunks.net>
2021-02-12 09:08:51 +0100

    refactor: extract narrative MDX boiler plate

--- src/pages/key-findings.tsx
+++ src/pages/key-findings.tsx
@@ -1,5 +1,3 @@
-import hydrate from "next-mdx-remote/hydrate";
-import renderToString from "next-mdx-remote/render-to-string";
 import React from "react";
 
 import Footnotes from "../components/footnotes";
@@ -7,28 +5,18 @@ import Layout from "../components/layout";
 import NarrativeContainer from "../components/narrative-container";
 import NarrativeTitle from "../components/narrative-title";
 import {keyFindings} from "../data";
-import {components} from "../mdx";
+import {hydrateNarrativePage, renderNarrativePage} from "../mdx";
 import {NarrativeProps} from "../types";
 
 export const getStaticProps = async () => {
   const details = await keyFindings();
-
-  const pageTitle = await renderToString(details.pageTitle, {components});
-  const body = await renderToString(details.body, {components});
-  const footnotes = details.footnotes
-    ? await renderToString(details.footnotes, {components})
-    : // eslint-disable-next-line unicorn/no-null
-      null;
+  const {pageTitle, body, footnotes} = await renderNarrativePage(details);
 
   return {props: {details: {pageTitle, body, footnotes}}};
 };
 
 const KeyFindings = ({details}: NarrativeProps) => {
-  const pageTitle = hydrate(details.pageTitle, {components});
-  const body = hydrate(details.body, {components});
-  const footnotes = details.footnotes
-    ? hydrate(details.footnotes, {components})
-    : undefined;
+  const {pageTitle, body, footnotes} = hydrateNarrativePage(details);
 
   return (
     <Layout>
