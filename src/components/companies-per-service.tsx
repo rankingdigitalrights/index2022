9ac9742134c5ed930c24cd230ee6f51f64f0e929
aeluteia <aeluteia@riseup.net>
2022-03-31 20:48:26 +0100

    skeleton companies-per-service perspective

---
+++ src/components/companies-per-service.tsx
@@ -0,0 +1,27 @@
+
+
+
+const CompaniesByService = () => {
+
+  console.log("companies by service")
+  
+  // props: 
+  // { service: 
+      // { company: 
+      // { total: xx,
+           // gov: xx,
+           // free: xx,
+           // priv: xx
+          //  },
+      // { company: 
+         // { total: xx,
+           // gov: xx,
+           // free: xx,
+           // priv: xx
+           //  },
+           // }
+           // }
+           // }
+          }
+
+          export default CompaniesByService
