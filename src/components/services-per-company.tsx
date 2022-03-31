e93015f0420ed903b67ffecf9e182d61ea0e950f
aeluteia <aeluteia@riseup.net>
2022-03-31 20:50:15 +0100

    skeleton services-per-company page

---
+++ src/components/services-per-company.tsx
@@ -0,0 +1,22 @@
+
+
+const ServicesByCompany = () => {
+console.log('services by company')
+}
+
+// Props = s
+// company: { service: 
+              // { total: xx, 
+                // gov: xx, 
+                // free: xx, 
+                // priv: xx
+                // }
+            // service: 
+              // { total: xx, 
+              // gov: xx, 
+              // free: xx, 
+              // priv: xx
+            // }
+
+
+            export default ServicesByCompany
