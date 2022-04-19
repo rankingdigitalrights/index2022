dffcd1efded3704cef1242cd843806afb1465672
Christo <christo@cryptodrunks.net>
2022-04-19 08:22:16 +0200

    refactor: fix formatting

--- src/pages/companies/[slug].tsx
+++ src/pages/companies/[slug].tsx
@@ -234,7 +234,6 @@ const CompanyPage = ({
                   </li>
                 )}
 
-
                 {meta.marketCap && meta.marketCapDate && (
                   <li className="pb-0">
                     <span className="font-bold">Market cap:</span>{" "}
@@ -275,9 +274,12 @@ const CompanyPage = ({
                   </li>
                 )}
                 <li className="pt-3 pb-0">
-                  <span className="">Read more about how stock structures can
-                  be a <a href="https://rankingdigitalrights.org/mini-report/its-time-to-bring-down-the-barriers-blocking-shareholders-on-human-rights/">barrier
-                  to shareholder participation</a></span>
+                  <span className="">
+                    Read more about how stock structures can be a{" "}
+                    <a href="https://rankingdigitalrights.org/mini-report/its-time-to-bring-down-the-barriers-blocking-shareholders-on-human-rights/">
+                      barrier to shareholder participation
+                    </a>
+                  </span>
                 </li>
 
                 <li className="pt-3 pb-0">
@@ -287,7 +289,10 @@ const CompanyPage = ({
 
                 {meta.firstServiceEval && (
                   <li className="pt-3 pb-0">
-                    <span className="text-sm">*In 2022, we evaluated {meta.firstServiceEval} for the first time.</span>{" "}
+                    <span className="text-sm">
+                      *In 2022, we evaluated {meta.firstServiceEval} for the
+                      first time.
+                    </span>{" "}
                   </li>
                 )}
               </ul>
@@ -341,13 +346,16 @@ const CompanyPage = ({
               <h2 className="text-prissian mt-8 mb-6">Scores since 2017</h2>
 
               <CompanyYearOverYearChart data={yearOverYear} />
-              <div className="text-sm font-serif mt-6">Most companies’ scores dropped
-              between 2019 and 2020 with the inclusion of our new indicators
-              on targeted advertising and algorithmic systems.
-              To learn more, please visit our <a href="https://rankingdigitalrights.org/methods-and-standards/#methodology-archive">Methodology development archive.</a>
+              <div className="text-sm font-serif mt-6">
+                Most companies’ scores dropped between 2019 and 2020 with the
+                inclusion of our new indicators on targeted advertising and
+                algorithmic systems. To learn more, please visit our{" "}
+                <a href="https://rankingdigitalrights.org/methods-and-standards/#methodology-archive">
+                  Methodology development archive.
+                </a>
               </div>
             </div>
-            </section>
+          </section>
         )}
       </div>
 
