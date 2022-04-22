c1c1ed9391bcc151c6c511684fc64fc8c386fd1d
Christo <christo@cryptodrunks.net>
2022-04-22 16:20:49 +0200

    refactor: improve explore container styling

--- src/components/explore-container.tsx
+++ src/components/explore-container.tsx
@@ -3,8 +3,6 @@ import React from "react";
 
 import Donate from "./donate";
 
-export const containerWidth = "md:w-10/12 lg:w-8/12 xl:w-8/12 2xl:w-7/12";
-
 interface ContainerProps {
   children: React.ReactNode;
   className?: string;
@@ -29,44 +27,16 @@ const ExploreContainer = ({
     children: contents,
     className,
   }: ContainerProps) => {
-    return (
-      <Container
-        className={c(
-          "px-3 md:px-16 xl:px-28 2xl:px-24",
-          containerWidth,
-          className,
-        )}
-      >
-        {contents}
-      </Container>
-    );
+    return <Container className={c(className)}>{contents}</Container>;
   };
 
   return (
     <div>
       <div className="flex flex-col justify-around py-3 md:py-12">
-        <div className="relative pb-0">
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div
-              className={c(
-                "explore-container w-full h-full top-0 z-10 mx-3 md:mx-auto",
-                "px-3 md:px-16 xl:px-28 2xl:px-24",
-              )}
-            />
-          </div>
-
-          <div className="relative z-10">
-            {children({Container: ContainerWrapper})}
-          </div>
-        </div>
+        <div className="pb-0">{children({Container: ContainerWrapper})}</div>
 
         {hasDonate && (
-          <Donate
-            className={c(
-              "narrative-container relative mx-3 md:mx-auto mt-3 md:mt-12",
-              containerWidth,
-            )}
-          />
+          <Donate className="explore-relative mx-3 md:mx-auto mt-3 md:mt-12" />
         )}
       </div>
     </div>
