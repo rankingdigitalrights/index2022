2790b6e6b40837fc838b49d067f6d25fec898160
Christo <christo@cryptodrunks.net>
2022-04-22 18:10:51 +0200

    remove width restrictions of charts

--- src/components/lenses.jsx
+++ src/components/lenses.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import CompanySelector from "./company-selector-simple";
 import FlipAxis from "./flip-axis";
 import LensCompanyChart from "./lens-company-chart";
@@ -6,7 +6,7 @@ import LensScoreChart from "./lens-score-chart";
 import NarrativeTitle from "./narrative-title";
 
 const LensCharts = (props) => {
-  const { companySelectors, indicatorLenses, indicatorCompanyLenses } = props
+  const {companySelectors, indicatorLenses, indicatorCompanyLenses} = props;
   const [selectedCompanies, setSelectedCompanies] = useState([]);
   const [lensChart, setLensCharts] = useState(true);
 
@@ -23,37 +23,31 @@ const LensCharts = (props) => {
       <NarrativeTitle title="Lenses (in beta)" />
 
       <p className="pb-3">
-        Lenses are a new look at our data that let us create scores
-        from groups of indicators across all three of our main
-        categories: governance, freedom of expression and information,
-        and privacy. Below we’ve created two Lenses from indicators
-        related to algorithmic transparency and targeted advertising,
-        so that we can illuminate companies’ lag in these two policy
-        areas.
+        Lenses are a new look at our data that let us create scores from groups
+        of indicators across all three of our main categories: governance,
+        freedom of expression and information, and privacy. Below we’ve created
+        two Lenses from indicators related to algorithmic transparency and
+        targeted advertising, so that we can illuminate companies’ lag in these
+        two policy areas.
       </p>
 
       <ul>
         <li>
-          <span className="font-bold">
-            Algorithmic transparency -
-          </span>{" "}
-          This Lens groups indicators that ask questions about whether
-          companies make a commitment to human rights in the
-          development and deployment of their algorithmic systems,
-          conduct human rights impact assessments on those systems,
-          make related policies accessible, disclose how they use
-          algorithms to curate, recommend, and rank content, and how
-          they use algorithms to collect, process, and make inferences
-          about user data.
+          <span className="font-bold">Algorithmic transparency -</span> This
+          Lens groups indicators that ask questions about whether companies make
+          a commitment to human rights in the development and deployment of
+          their algorithmic systems, conduct human rights impact assessments on
+          those systems, make related policies accessible, disclose how they use
+          algorithms to curate, recommend, and rank content, and how they use
+          algorithms to collect, process, and make inferences about user data.
         </li>
 
         <li>
-          <span className="font-bold">Targeted advertising -</span>{" "}
-          This Lens groups indicators that ask whether companies
-          conduct human rights impact assessments on their targeted
-          advertising systems, as well as whether they clearly
-          disclose rules around ad targeting and how those rules are
-          enforced.
+          <span className="font-bold">Targeted advertising -</span> This Lens
+          groups indicators that ask whether companies conduct human rights
+          impact assessments on their targeted advertising systems, as well as
+          whether they clearly disclose rules around ad targeting and how those
+          rules are enforced.
         </li>
       </ul>
 
@@ -72,21 +66,19 @@ const LensCharts = (props) => {
         />
       </div>
 
-      <div className="relative mx-auto md:w-10/12 lg:w-8/12 xl:w-8/12 2xl:w-7/12">
-        {lensChart ? (
-          <LensScoreChart
-            className="w-full"
-            lenses={indicatorLenses}
-            companyList={selectedCompanies}
-          />
-        ) : (
-          <LensCompanyChart
-            className="w-full"
-            companyLenses={indicatorCompanyLenses}
-            companyList={selectedCompanies}
-          />
-        )}
-      </div>
+      {lensChart ? (
+        <LensScoreChart
+          className="w-full"
+          lenses={indicatorLenses}
+          companyList={selectedCompanies}
+        />
+      ) : (
+        <LensCompanyChart
+          className="w-full"
+          companyLenses={indicatorCompanyLenses}
+          companyList={selectedCompanies}
+        />
+      )}
     </div>
   );
 };
