0181416acf95929745f7c11da1f24686e39508d1
Christo <christo@cryptodrunks.net>
2022-04-24 23:59:33 +0200

    refactor: extract service company chart into component with highlighting

---
+++ src/components/service-company-chart.tsx
@@ -0,0 +1,91 @@
+/* eslint unicorn/no-null: off */
+import c from "clsx";
+import React, {useState} from "react";
+
+import {useChartResize} from "../hooks";
+import {CompanyRank, ServiceKind} from "../types";
+import {mapIcon} from "./evaluated-service";
+import PercentageBar from "./percentage-bar";
+import RankScore from "./rank-score";
+
+type ChartRanking = Pick<CompanyRank, "score"> & {
+  service: string;
+  kind: ServiceKind;
+};
+
+interface ServiceCompanyChartProps {
+  ranking: ChartRanking[];
+  className?: string;
+  chartHeight?: number;
+}
+
+const ServiceCompanyChart = ({
+  ranking,
+  className,
+  chartHeight = 10,
+}: ServiceCompanyChartProps) => {
+  const [chartRef, chartWidth] = useChartResize();
+  const [highlightedService, setHighlightedService] = useState<
+    string | undefined
+  >();
+
+  return (
+    <div className={c("flex flex-col w-full font-sans", className)}>
+      {ranking.map(({service, kind, score}, idx) => {
+        const isHighlighted = service === highlightedService;
+
+        const highlightedTextClassName = {
+          "text-black": !isHighlighted,
+          "text-prissian": isHighlighted,
+        };
+
+        return (
+          <div
+            key={`home-rank-${kind}-${service}`}
+            className="flex items-center text-sm mb-1"
+            onMouseEnter={() => setHighlightedService(service)}
+            onMouseLeave={() => setHighlightedService(undefined)}
+          >
+            <div className="grow-0 shrink-0 flex items-center">
+              <div className="h-8 w-8 mt-1.5">{mapIcon(kind, false)}</div>
+
+              <div className={c("ml-1 w-28 text-sm", highlightedTextClassName)}>
+                {service}
+              </div>
+            </div>
+
+            <div
+              ref={idx === 0 ? chartRef : null}
+              className="grow flex items-center ml-2 border"
+            >
+              <svg
+                version="1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height={chartHeight}
+                transform="translate(0, 0)"
+                aria-label={`Score bar for ${service}: ${score}`}
+              >
+                <PercentageBar
+                  value={score}
+                  width={chartWidth}
+                  height={chartHeight}
+                  className="text-prissian"
+                />
+              </svg>
+            </div>
+
+            <div className="relative shrink-0 w-9 ml-2">
+              <RankScore
+                className={c(highlightedTextClassName)}
+                score={score}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ServiceCompanyChart;
--- src/components/services-by-company.jsx
+++ src/components/services-by-company.jsx
@@ -1,103 +1,70 @@
 import Link from "next/link";
 import React from "react";
 
-import {useChartResize} from "../hooks";
-import {mapIcon} from "./evaluated-service";
-import PercentageBar from "./percentage-bar";
 import PillHeader from "./pill-header";
+import ServiceCompanyChart from "./service-company-chart";
 
 const ServicesByCompany = ({category, companies}) => {
-  const [chartRef, chartWidth] = useChartResize();
-  const chartHeight = 10;
-
-  const categoryClassName = {
-    "text-cat-governance": category === "governance",
-    "text-cat-freedom": category === "freedom",
-    "text-cat-privacy": category === "privacy",
-    "text-prissian": category === "total",
-  };
-
-  const chartHeader = (companyName, companyId) => {
-    return (
-      <PillHeader>
-        <Link passHref href={`/companies/${companyId}`}>
-          <a>{companyName}</a>
-        </Link>
-      </PillHeader>
-    );
-  };
-
-  const serviceIcon = (serviceKind) => {
-    return mapIcon(serviceKind, false);
-  };
-
-  const chartRow = (icon, serviceName, score, idx) => {
-    // eslint-disable-next-line unicorn/no-null
-    const ref = idx === 0 ? chartRef : null;
-    return (
-      <div
-        key={`chart-row-${category}-${serviceName}`}
-        className="flex items-center space-x-1 pr-1.5 pl-1.5"
-      >
-        <div className="flex-none justify-center w-8">{icon}</div>
-
-        <div className="flex-none w-28 text-sm">{serviceName}</div>
-
-        <div ref={ref} className="grow flex items-center ml-2">
-          <svg
-            version="1"
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height={chartHeight}
-            transform="translate(0, 0)"
-            aria-label={`Score bar for ${serviceName}: ${score}`}
-          >
-            <PercentageBar
-              value={score}
-              width={chartWidth}
-              height={chartHeight}
-              className={categoryClassName}
-            />
-          </svg>
-        </div>
-        <span className="shrink-0 text-right w-9 pl-1 pr-1 select-none float-right text-prissian text-xs">
-          {score}%
-        </span>
-      </div>
-    );
-  };
-
-  const chartBlock = (companiesArr, cat) => {
-    return (
-      <div className="space-y-5">
-        {companiesArr.map((company, idx) => {
-          return (
-            <div
-              key={`chartBlock-${company.name}-${company.id}`}
-              className="flex flex-col space-y-5"
-            >
-              {chartHeader(company.name, company.id)}
-              {company.services.map((service) => {
-                const sIcon = serviceIcon(service.kind);
-                const score = service.categoryScore[cat];
-                return chartRow(sIcon, service.name, score, idx);
-              })}
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-
   const divider = Math.ceil(companies.length / 2);
 
   return (
     <div className="flex flex-col space-y-5 md:space-y-0 md:space-x-8 md:flex-row font-sans">
       <div className="w-full md:w-1/2">
-        {chartBlock(companies.slice(0, divider), category)}
+        <div className="space-y-5">
+          {companies.slice(0, divider).map(({id, name, services}) => {
+            return (
+              <div
+                key={`services-by-company-${name}`}
+                className="flex flex-col space-y-5"
+              >
+                <PillHeader>
+                  <Link passHref href={`/companies/${id}`}>
+                    <a>{name}</a>
+                  </Link>
+                </PillHeader>
+
+                <ServiceCompanyChart
+                  ranking={services.map((service) => {
+                    return {
+                      service: service.name,
+                      kind: service.kind,
+                      score: service.categoryScore[category],
+                    };
+                  })}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
+
       <div className="w-full md:w-1/2">
-        {chartBlock(companies.slice(divider), category)}
+        <div className="space-y-5">
+          {companies.slice(divider).map(({id, name, services}) => {
+            return (
+              <div
+                key={`services-by-company-${name}`}
+                className="flex flex-col space-y-5"
+              >
+                <PillHeader>
+                  <Link passHref href={`/companies/${id}`}>
+                    <a>{name}</a>
+                  </Link>
+                </PillHeader>
+
+                <ServiceCompanyChart
+                  ranking={services.map((service) => {
+                    return {
+                      service: service.name,
+                      kind: service.kind,
+                      score: service.categoryScore[category],
+                    };
+                  })}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
