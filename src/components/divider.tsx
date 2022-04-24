2fbff2462234c00ff0a963ef85785d9da99c4ee2
Christo <christo@cryptodrunks.net>
2022-04-24 13:45:22 +0200

    refactor: improve spacing for data explorer on mobile

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Divider = () => {
-  return <div className="w-full my-20 border-b-2 border-beige" />;
+  return (
+    <div className="w-full my-8 lg:my-12 xl:my-20 border-b-2 border-beige" />
+  );
 };
 
 export default Divider;
