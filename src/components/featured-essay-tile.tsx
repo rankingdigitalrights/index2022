0285809a41f2abb77e8b201451d5ce66fc6766e6
Christo <christo@cryptodrunks.net>
2022-04-26 12:07:32 +0200

    feat: add subtext to featured essay tiles

--- src/components/featured-essay-tile.tsx
+++ src/components/featured-essay-tile.tsx
@@ -5,6 +5,7 @@ import Image from "./image";
 
 interface FeaturedEssayTileProps {
   title: string;
+  subText?: string;
   href: string;
   src: string;
   className?: string;
@@ -12,20 +13,30 @@ interface FeaturedEssayTileProps {
 
 const FeaturedEssayTile = ({
   title,
+  subText,
   href,
   src,
   className,
 }: FeaturedEssayTileProps) => {
   return (
-    <div className={c("flex flex-col space-y-6", className)}>
-      <Image
-        src={src}
-        alt={title}
-        className="aspect-ratio overflow-hidden md:h-72"
-      />
-      <a href={href} className="text-black font-sans">
-        {title}
+    <div
+      className={c(
+        "flex flex-col space-y-2 group block hover-opacity",
+        className,
+      )}
+    >
+      <a href={href} className="flex flex-col space-y-6 text-black font-sans">
+        <Image
+          src={src}
+          alt={title}
+          className="aspect-video overflow-hidden md:h-72 pointer-events-none group-hover:opacity-75"
+        />
+
+        <span className="text-md leading-6">{title}</span>
       </a>
+      {subText && (
+        <p className="font-sans text-sm pointer-events-none">{subText}</p>
+      )}
     </div>
   );
 };
--- src/components/home-featured-essays.tsx
+++ src/components/home-featured-essays.tsx
@@ -9,28 +9,32 @@ const HomeFeaturedEssays = () => {
         Featured Essays
       </h3>
 
-      <div className="grid grid-rows-4 grid-cols-1 md:grid-rows-2 md:grid-cols-2 xl:grid-rows-1 xl:grid-cols-4 gap-5 mt-8">
+      <div className="flex flex-col sm:grid sm:grid-rows-4 sm:grid-cols-1 md:grid-rows-2 md:grid-cols-2 xl:grid-rows-1 xl:grid-cols-4 gap-5 mt-8">
         <Tile
           title="Key Findings"
+          subText="Key Findings from the 2022 RDR Big Tech Scorecard."
           href="https://rankingdigitalrights.org/mini-report/key-findings-2022/"
           src="essays/key-findings.jpg"
           className="w-full overflow-hidden"
         />
         <Tile
           title="We Must Govern Online Ads"
+          subText="For a global internet that supports and sustains human rights, we need a global online advertising ecosystem that does the same thing."
           href="https://rankingdigitalrights.org/mini-report/we-must-govern-online-ads"
           src="essays/online-ads.jpg"
           className="w-full overflow-hidden"
         />
         <Tile
           title="Empowering Big Tech Shareholders"
+          subText="It’s time to bring down the barriers blocking shareholders on human rights."
           href="https://rankingdigitalrights.org/mini-report/its-time-to-bring-down-the-barriers-blocking-shareholders-on-human-rights"
           src="essays/big-tech.jpg"
           className="w-full overflow-hidden"
         />
         <Tile
           title="Why Won't Chinese Companies Talk to Us?"
-          href="https://rankingdigitalrights.org/2022/04/20/why-wont-chinese-companies-talk-to-us"
+          subText="Chinese companies may want to engage with civil society. But in the era of Xi Jinping, it’s not that simple."
+          href="https://rankingdigitalrights.org/2022/04/27/why-wont-chinese-companies-talk-to-us"
           src="essays/chinese-companies.jpg"
           className="w-full overflow-hidden"
         />
